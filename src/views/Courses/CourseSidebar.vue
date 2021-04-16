f297cc0dc5c40cc325ad86508463285a0f271315
Amin Mohamadi <40574511+aminmohamadi@users.noreply.github.com>
2021-04-16 06:53:02 +0430

    forth commit

---
+++ src/views/Courses/CourseSidebar.vue
@@ -0,0 +1,326 @@
+<template>
+  <vs-sidebar click-not-close position-right parent="body" default-index="1" color="primary"
+              class="add-new-data-sidebar items-no-padding" spacer v-model="isSidebarActiveLocal">
+    <div class="mt-6 flex items-center justify-between px-6">
+      <h4>{{ Object.entries(this.data).length === 0 ? 'افزودن' : 'ویرایش' }} دوره</h4>
+      <feather-icon icon="XIcon" @click.stop="isSidebarActiveLocal = false" class="cursor-pointer"></feather-icon>
+    </div>
+    <vs-divider class="mb-0"></vs-divider>
+
+    <perfect-scrollbar>
+
+      <div class="p-6">
+
+        <!-- Product Image -->
+        <template v-if="dataImageUrl == null">
+          <!-- Image Container -->
+          <div class="img-container w-64 mx-auto flex items-center justify-center">
+            <img :src="defaultImage" alt="img" class="responsive">
+          </div>
+          <div class="modify-img flex justify-between mt-5">
+            <input type="file" class="hidden" ref="updateImgInput" @change="updateCurrImg" accept="image/*">
+            <vs-button class="mr-4" type="flat" @click="$refs.updateImgInput.click()">افزودن تصویر</vs-button>
+          </div>
+
+        </template>
+        <template v-else>
+          <!-- Image Container -->
+          <div class="img-container w-64 mx-auto flex items-center justify-center">
+            <img :src="dataImageUrl" alt="img" class="responsive" ref="dataAvatar">
+          </div>
+
+          <!-- Image upload Buttons -->
+          <!-- Image upload Buttons -->
+          <div class="modify-img flex justify-between mt-5">
+            <input type="file" class="hidden" ref="updateImgInput" @change="updateCurrImg" accept="image/*">
+            <vs-button class="mr-4" type="flat" @click="$refs.updateImgInput.click()">Update Image</vs-button>
+            <vs-button type="flat" color="#999" @click="dataImageUrl = null">Remove Image</vs-button>
+          </div>
+        </template>
+        <div class="vx-row">
+          <div class="vs-col col-12 mt-5 w-1/2 px-2">
+            <vs-input label="عنوان" v-model="dataTitle" class="w-full" name="item-title" v-validate="'required'"/>
+            <span class="text-danger text-sm" v-show="errors.has('item-title')">{{
+                errors.first('item-title')
+              }}</span>
+          </div>
+          <div class="vs-col col-12 mt-5 w-1/2 px-2">
+            <vs-input label="توضیح" v-model="dataDescription" v-validate="'required'" class="w-full"
+                      name="item-description"/>
+            <span class="text-danger text-sm" v-show="errors.has('item-description')">{{
+                errors.first('item-description')
+              }}</span>
+          </div>
+        </div>
+        <div class="vx-row">
+          <div class="vs-col col-12 mt-5 w-1/3 px-2">
+            <vs-input label="کلمات کلیدی" v-model="dataTags" class="w-full" name="item-tags" v-validate="'required'"/>
+            <span class="text-danger text-sm" v-show="errors.has('item-tags')">{{ errors.first('item-tags') }}</span>
+          </div>
+          <div class="vs-col col-12 mt-5 w-1/3 px-2">
+            <vs-select label="نوع دوره" v-model="dataType" v-validate="'required'" class="w-full"
+                       name="item-phone">
+              <vs-select-item value="" text="انتخاب کنید" label="انتخاب کنید" />
+              <vs-select-item value="free" text="رایگان" label="رایگان" />
+              <vs-select-item value="cash" text="نقدی" label="نقدی" />
+              <vs-select-item value="vip" text="اعضای ویژه" label="اعضای ویژه" />
+            </vs-select>
+            <span class="text-danger text-sm" v-show="errors.has('item-phone')">{{
+                errors.first('item-phone')
+              }}</span>
+          </div>
+          <div class="vs-col col-12 mt-5 w-1/3 px-2">
+            <vs-input label="قیمت" v-model="dataPrice" class="w-full" name="item-price" v-validate="'required'"/>
+            <span class="text-danger text-sm" v-show="errors.has('item-price')">{{ errors.first('item-price') }}</span>
+          </div>
+
+        </div>
+        <div class="vx-row">
+          <div class="vs-col col-12 mt-5 w-full px-2">
+            <template>
+              <template>
+                <div class="quill-wrap">
+                  <vue-editor id="editor" useCustomImageHandler @image-added="handleImageAdded" v-model="dataBody"/>
+                </div>
+              </template>
+            </template>
+          </div>
+        </div>
+
+      </div>
+    </perfect-scrollbar>
+
+    <div class="flex flex-wrap items-center p-6" slot="footer">
+      <vs-button class="mr-6" @click="submitData" :disabled="!isFormValid">ثبت</vs-button>
+      <vs-button type="border" color="danger" @click="isSidebarActiveLocal = false">لغو</vs-button>
+    </div>
+  </vs-sidebar>
+</template>
+
+<script>
+import axios from './../../http/axios/index'
+import {VueEditor, Quill} from 'vue2-editor'
+
+export default {
+  props: {
+    isSidebarActive: {
+      typuired: true
+    },
+    data: {
+      type: Object,
+      default: () => {
+      }
+    }
+  },
+  data () {
+    return {
+      dataId :null,
+      dataTitle :null,
+      dataDescription :null,
+      dataBody :null,
+      dataTags :null,
+      dataPrice :null,
+      dataType :null,
+      dataImageUrl :null,
+      file:null,
+      defaultImage: 'http://shop.test/images/defaults/ArticleDefault.jpg',
+      settings: { // perfectscrollbar settings
+        maxScrollbarLength: 60,
+        wheelSpeed: .60
+      },
+      editorOption: {
+        imageUpload : this.handleImageAdded
+      }
+    }
+  },
+  watch: {
+    isSidebarActive (val) {
+      if (!val) return
+      if (Object.entries(this.data).length === 0) {
+        this.initValues()
+        this.$validator.reset()
+      } else {
+        const {
+          id,
+          title,
+          description,
+          body,
+          tags,
+          price,
+          type,
+          image_url
+        } = JSON.parse(JSON.stringify(this.data))
+        this.dataId = id
+        this.dataTitle = title
+        this.dataDescription = description
+        this.dataBody = body
+        this.dataTags = tags
+        this.dataPrice = price
+        this.dataType = type
+        this.dataImageUrl = image_url
+        this.initValues()
+      }
+      // Object.entries(this.data).length === 0 ? this.initValues() : { this.dataId, this.dataName, this.dataCategory, this.dataOrder_status, this.dataPrice } = JSON.parse(JSON.stringify(this.data))
+    }
+  },
+  computed: {
+    isFormValid () {
+      return !this.errors.any() && this.dataTitle && this.dataDescription && this.dataBody
+          && this.dataTags  && this.dataPrice  && this.dataType && this.dataImageUrl
+    },
+    isSidebarActiveLocal: {
+      get () {
+        return this.isSidebarActive
+      },
+      set (val) {
+        if (!val) {
+          this.$emit('closeSidebar')
+          // this.$validator.reset()
+          // this.initValues()
+        }
+      }
+    },
+
+    scrollbarTag () {
+      return this.$store.getters.scrollbarTag
+    }
+  },
+  methods: {
+    initValues () {
+      if (this.data.id) return
+      this.dataId = null
+      this.dataTitle = null
+      this.dataDescription = null
+      this.dataBody = null
+      this.dataTags = null
+      this.dataPrice = null
+      this.dataType = null
+      this.dataImageUrl = null
+    },
+    submitData () {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          let formData = new FormData();
+          formData.append('title',this.dataTitle)
+          formData.append('description',this.dataDescription)
+          formData.append('body',this.dataBody)
+          formData.append('tags',this.dataTags)
+          formData.append('type',this.dataType)
+          formData.append('price',this.dataPrice)
+          formData.append('image_url',this.dataImageUrl)
+          if (Object.entries(this.data).length === 0 ){
+            var customUrl = "/courses"
+            formData.append('avatar',this.file)
+
+          }
+          else {
+            customUrl = "courses/"+ this.dataId
+            formData.append('_method','patch')
+            formData.append('avatar',this.dataAvatar)
+          }
+          axios.post(customUrl,formData).then((response) => {
+            this.$parent.courses = response.data.courses
+            this.$vs.notify({
+              title: response.data.title,
+              text: response.data.message,
+              iconPack: 'feather',
+              icon: 'icon-'+response.data.icon+'-circle',
+              color: response.data.status
+            })
+          })
+              .catch((error) => {
+                this.$vs.notify({
+                  title: 'خطا',
+                  text: error,
+                  iconPack: 'feather',
+                  icon: 'icon-alert-circle',
+                  color: 'danger'
+                })
+              })
+          this.$emit('closeSidebar')
+          this.initValues()
+        }
+      })
+    },
+    updateCurrImg (input) {
+      if (input.target.files && input.target.files[0]) {
+        const reader = new FileReader()
+        reader.onload = e => {
+          this.dataImageUrl = e.target.result
+          this.file = this.$refs.updateImgInput.files[0];
+          this.$refs.dataAvatar.src = this.$refs.updateImgInput.files[0];
+        }
+        reader.readAsDataURL(input.target.files[0])
+      }
+    },
+    handleImageAdded: function (file, Editor, cursorLocation, resetUploader) {
+      // An example of using FormData
+      // NOTE: Your key could be different such as:
+      // formData.append('file', file)
+
+      var formData = new FormData()
+      formData.append('image', file)
+
+      axios({
+        url: 'upload',
+        method: 'POST',
+        data: formData
+      })
+          .then(result => {
+            let url = result.data.url // Get url from response
+            Editor.insertEmbed(cursorLocation, 'image', url)
+            resetUploader()
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    }
+  },
+  components: {
+    VueEditor
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.add-new-data-sidebar {
+  ::v-deep .vs-sidebar--background {
+    z-index: 52010;
+  }
+
+  ::v-deep .vs-sidebar {
+    z-index: 52010;
+    width: 400px;
+    max-width: 90vw;
+
+    .img-upload {
+      margin-top: 2rem;
+
+      .con-img-upload {
+        padding: 0;
+      }
+
+      .con-input-upload {
+        width: 100%;
+        margin: 0;
+      }
+    }
+  }
+}
+
+.scroll-area--data-list-add-new {
+  // height: calc(var(--vh, 1vh) * 100 - 4.3rem);
+  height: calc(var(--vh, 1vh) * 100 - 16px - 45px - 82px);
+
+  &:not(.ps) {
+    overflow-y: auto;
+  }
+}
+
+.ps {
+  height: 100%;
+}
+.ql-editor .ql-blank {
+  height: 400px;
+}
+</style>
---
+++ src/views/Courses/Courses.vue
@@ -0,0 +1,384 @@
+<template>
+  <div id="data-list-list-view" class="data-list-container">
+    <vx-card ref="filterCard" title="جست و جو" class="users-list-filters mb-8">
+      <div class="vx-row">
+        <div class="vx-col md:w-1/3 sm:w-1/3 w-full">
+          <label for="name" class="text-sm opacity-75">نام</label>
+          <vs-input id="name" v-model="searchQuery.firstName" class="w-full mt-4" placeholder="نام"/>
+        </div>
+        <div class="vx-col md:w-1/3 sm:w-1/3 w-full">
+          <label for="lastName" class="text-sm opacity-75">نام</label>
+          <vs-input id="lastName" v-model="searchQuery.lastName" class="w-full mt-4" placeholder=" نام خانوادگی"/>
+        </div>
+        <div class="vx-col md:w-1/3 sm:w-1/3 w-full">
+          <label for="email" class="text-sm opacity-75">ایمیل</label>
+          <vs-input id="email" v-model="searchQuery.email" class="w-full mt-4" placeholder="ایمیل"/>
+        </div>
+        <div class="vx-col md:w-1/3 sm:w-1/3 w-full">
+          <label for="phone" class="text-sm opacity-75">موبایل</label>
+          <vs-input id="phone" v-model="searchQuery.phone" class="w-full mt-4" placeholder="موبایل"/>
+        </div>
+        <div class="vx-col md:w-1/3 sm:w-1/3 w-full">
+          <label for="gender" class="text-sm opacity-75">جنسیت</label>
+          <vs-select id="gender" v-model="searchQuery.gender" class="w-full mt-4">
+            <vs-select-item label="همه" text="همه" value=""/>
+            <vs-select-item label="آقا" text="آقا" value="man"/>
+            <vs-select-item label="خانم" text="خانم" value="woman"/>
+          </vs-select>
+        </div>
+      </div>
+    </vx-card>
+    <data-view-sidebar :isSidebarActive="addNewDataSidebar" @closeSidebar="toggleDataSidebar" :data="sidebarData"/>
+    <vx-card actionButtons @refresh="fetch" title="همه نقش ها" class="course-list mb-8">
+      <vs-table ref="table" multiple v-model="selected" pagination :max-items="itemsPerPage" :data="resultQuery"
+                noDataText="موردی برای نمایش وجود ندارد">
+
+        <div slot="header" class="flex flex-wrap-reverse items-center flex-grow justify-between">
+
+          <div class="flex flex-wrap-reverse items-center data-list-btn-container">
+
+            <!-- ADD NEW -->
+            <button class="p-3 mb-4 mr-4 rounded-lg cursor-pointer flex items-center justify-center text-lg font-medium text-base
+                          text-primary border border-solid border-primary " style="background-color: #fff"
+                    @click="addNewData">
+              <span class="">افزودن</span>
+              <feather-icon icon="PlusIcon" svgClasses="h-4 w-4"/>
+            </button>
+          </div>
+
+          <!-- ITEMS PER PAGE -->
+          <vs-dropdown vs-trigger-click class="cursor-pointer mb-4 mr-4 items-per-page-handler ">
+            <div
+                class="text-base text-primary p-4 border border-solid border-primary d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium">
+              <span class="mr-2">{{
+                  currentPage * itemsPerPage - (itemsPerPage - 1)
+                }} - {{
+                  resultQuery.length - currentPage * itemsPerPage > 0 ? currentPage * itemsPerPage : resultQuery.length
+                }} از {{ queriedItems }} آیتم</span>
+              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4"/>
+            </div>
+            <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
+            <vs-dropdown-menu>
+
+              <vs-dropdown-item @click="itemsPerPage=4">
+                <span>4</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="itemsPerPage=10">
+                <span>10</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="itemsPerPage=15">
+                <span>15</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="itemsPerPage=20">
+                <span>20</span>
+              </vs-dropdown-item>
+            </vs-dropdown-menu>
+          </vs-dropdown>
+        </div>
+
+        <template class="flex  flex-row-reverse" slot="thead">
+          <vs-th class="text-center" sort-key="title">عنوان</vs-th>
+          <vs-th class="text-center" sort-key="author">نویسنده</vs-th>
+          <vs-th class="text-center" sort-key="created_at">تاریخ انتشار</vs-th>
+          <vs-th class="text-center" sort-key="type">نوع</vs-th>
+          <vs-th class="text-center" sort-key="price">قیمت</vs-th>
+          <vs-th class="text-center">ویدیو های دوره</vs-th>
+          <vs-th class="text-center">عملیات</vs-th>
+        </template>
+
+        <template slot-scope="{data}">
+          <tbody>
+          <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
+            <vs-td class="text-center">
+              <p class="product-name font-medium truncate">{{ tr.title }}</p>
+            </vs-td>
+            <vs-td class="text-center">
+              <p class="product-category">{{ tr.author }}</p>
+            </vs-td>
+            <vs-td class="text-center">
+              <p class="product-category">{{ tr.created_at }}</p>
+            </vs-td>
+
+            <vs-td class="text-center">
+              <p class="product-category">{{ tr.type }}</p>
+            </vs-td>
+            <vs-td class="text-center">
+              <p class="product-category">{{ tr.price }}</p>
+            </vs-td>
+            <vs-td class="text-center">
+              <p class="product-category">ویدیو های دوره</p>
+            </vs-td>
+            <vs-td class="whitespace-no-wrap text-center">
+              <feather-icon icon="EditIcon" svgClasses="w-5 h-5 hover:text-warning stroke-current"
+                            @click.stop="editData(tr)"/>
+              <feather-icon icon="TrashIcon" svgClasses="w-5 h-5 hover:text-danger stroke-current"
+                            class="ml-2" @click.stop="confirmDeleteRecord(tr.id)"/>
+            </vs-td>
+
+          </vs-tr>
+          </tbody>
+        </template>
+      </vs-table>
+    </vx-card>
+
+  </div>
+</template>
+
+<script>
+import DataViewSidebar from './CourseSidebar.vue'
+import axios from './../../http/axios/index'
+import vSelect from 'vue-select'
+import Button from '../components/vuesax/button/Button'
+
+export default {
+  components: {
+    Button,
+    DataViewSidebar,
+    vSelect
+  },
+  data () {
+    return {
+      searchQuery: {
+        firstName: '',
+        lastName:'',
+        email:'',
+        gender:'',
+        phone:'',
+        role:''
+      },
+      selected: [],
+      courses: [],
+      itemsPerPage: 4,
+      isMounted: false,
+      addNewDataSidebar: false,
+      sidebarData: {}
+
+    }
+  },
+  computed: {
+
+    currentPage () {
+      if (this.isMounted) {
+        return this.$refs.table.currentx
+      }
+      return 0
+    },
+    queriedItems () {
+      return this.$refs.table ? this.$refs.table.queriedResults.length : this.courses.length
+    },
+    resultQuery () {
+      if (this.searchQuery.firstName !== '' || this.searchQuery.lastName !== '' || this.searchQuery.email !== ''
+          || this.searchQuery.role !== '' || this.searchQuery.gender !== '') {
+        return this.courses.filter((item) => {
+          return this.searchQuery.firstName.toLowerCase().split(' ').every(v => item.first_name.toLowerCase().includes(v)) &&
+              this.searchQuery.lastName.toLowerCase().split(' ').every(v => item.last_name.toLowerCase().includes(v)) &&
+              this.searchQuery.email.toLowerCase().split(' ').every(v => item.email.toLowerCase().includes(v)) &&
+              this.searchQuery.phone.toLowerCase().split(' ').every(v => item.phone.toLowerCase().includes(v)) &&
+              this.searchQuery.role.toLowerCase().split(' ').every(v => item.role.toLowerCase().includes(v)) &&
+              this.searchQuery.gender.toLowerCase().split(' ').every(v => item.gender.toLowerCase().includes(v))
+        })
+      } else {
+        return this.courses
+      }
+    }
+  },
+  methods: {
+    addNewData () {
+      this.sidebarData = {}
+      this.toggleDataSidebar(true)
+    },
+    editData (data) {
+      this.sidebarData = data
+      this.toggleDataSidebar(true)
+    },
+    toggleDataSidebar (val = false) {
+      this.addNewDataSidebar = val
+    },
+    fetch (card) {
+      axios.get(`/courses`)
+          .then((response) => {
+            this.courses = response.data.courses
+            card.removeRefreshAnimation()
+
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+    },
+    confirmDeleteRecord (id) {
+      this.$vs.dialog({
+        type: 'confirm',
+        color: 'danger',
+        title: ` حذف شود؟`,
+        text: `در صورت حذف امکان بازگشت اطلاعات وجود ندارد`,
+        acceptText: 'حذف',
+        cancelText: 'لغو',
+        accept: this.deleteRecord,
+        parameters: id
+      })
+    },
+    deleteRecord (id) {
+      axios.delete(`courses/` + id, {
+        headers: {
+          'Authorization': 'Bearer' + localStorage.getItem('access_token')
+        }
+
+      })
+          .then((response) => {
+            this.courses = response.data.courses
+            this.$vs.notify({
+              title: response.data.title,
+              text: response.data.message,
+              iconPack: 'feather',
+              icon: 'icon-' + response.data.icon + '-circle',
+              color: response.data.status
+            })
+          })
+          .catch((error) => {
+            this.$vs.notify({
+              title: 'خطا',
+              text: error,
+              iconPack: 'feather',
+              icon: 'icon-alert-circle',
+              color: 'danger'
+            })
+          })
+    }
+  },
+  created () {
+    this.fetch()
+  },
+  mounted () {
+    this.isMounted = true
+
+  }
+}
+</script>
+
+<style lang="scss">
+#data-list-list-view {
+  .vs-con-table {
+
+    @media (max-width: 689px) {
+      .vs-table--search {
+        margin-left: 0;
+        max-width: unset;
+        width: 100%;
+
+        .vs-table--search-input {
+          width: 100%;
+        }
+      }
+    }
+
+    @media (max-width: 461px) {
+      .items-per-page-handler {
+        display: none;
+      }
+    }
+
+    @media (max-width: 341px) {
+      .data-list-btn-container {
+        width: 100%;
+
+        .dd-actions,
+        .btn-add-new {
+          width: 100%;
+          margin-right: 0 !important;
+        }
+      }
+    }
+
+    .product-name {
+      max-width: 23rem;
+    }
+
+    .vs-table--header {
+      display: flex;
+      flex-wrap: wrap;
+      margin-left: 1.5rem;
+      margin-right: 1.5rem;
+
+      > span {
+        display: flex;
+        flex-grow: 1;
+      }
+
+      .vs-table--search {
+        padding-top: 0;
+
+        .vs-table--search-input {
+          padding: 0.9rem 2.5rem;
+          font-size: 1rem;
+
+          & + i {
+            left: 1rem;
+          }
+
+          &:focus + i {
+            left: 1rem;
+          }
+        }
+      }
+    }
+
+    .vs-table {
+      border-collapse: separate;
+      border-spacing: 0 1.3rem;
+      padding: 0 1rem;
+
+      tr {
+        box-shadow: 0 4px 20px 0 rgba(0, 0, 0, .05);
+
+        td {
+          padding: 20px;
+
+          &:first-child {
+            border-top-left-radius: .5rem;
+            border-bottom-left-radius: .5rem;
+          }
+
+          &:last-child {
+            border-top-right-radius: .5rem;
+            border-bottom-right-radius: .5rem;
+          }
+        }
+
+        td.td-check {
+          padding: 20px !important;
+        }
+      }
+    }
+
+    .vs-table--thead {
+      th {
+        padding-top: 0;
+        padding-bottom: 0;
+
+        .vs-table-text {
+          text-transform: uppercase;
+          font-weight: 600;
+          text-align: center !important;
+        }
+      }
+
+      th.td-check {
+        padding: 0 15px !important;
+      }
+
+      tr {
+        background: none;
+        box-shadow: none;
+      }
+    }
+
+    .vs-table--pagination {
+      justify-content: center;
+    }
+  }
+}
+
+.vs-sidebar {
+  width: 84% !important;
+}
+</style>
